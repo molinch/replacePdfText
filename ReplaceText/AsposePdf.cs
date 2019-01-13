31b9b2909f3bed1e33240fdb323b99d55046ba7f
Fabien Molinet <fabien.molinet@gmail.com>
2019-01-13 16:22:47 +0100

    Replace with Aspose PDF for .NET Core

---
+++ ReplaceText/AsposePdf.cs
@@ -0,0 +1,23 @@
+﻿using Aspose.Pdf;
+using Aspose.Pdf.Text;
+
+namespace ReplaceText
+{
+    class AsposePdf
+    {
+        public static void Replace(string sourceFile, string destinationFile, string find, string replace)
+        {
+            var pdf = new Document(sourceFile);
+            var absorber = new TextFragmentAbsorber(find);
+            absorber.TextReplaceOptions = new TextReplaceOptions(TextReplaceOptions.ReplaceAdjustment.AdjustSpaceWidth);
+            pdf.Pages.Accept(absorber);
+
+            foreach (var fragment in absorber.TextFragments)
+            {
+                fragment.Text = replace;
+            }
+
+            pdf.Save(destinationFile);
+        }
+    }
+}
---
+++ ReplaceText/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ReplaceText
+{
+    class Program
+    {
+        private const string SourceFile = @"..\..\..\Documents\Hello.pdf";
+        private const string DestinationFile = @"..\..\..\Documents\Hello-Fabien.pdf";
+        private const string Find = "##732##";
+        private const string Replace = "Fabien Molinet";
+
+        static void Main(string[] args)
+        {
+            AsposePdf.Replace(SourceFile, DestinationFile, Find, Replace);
+        }
+    }
+}
